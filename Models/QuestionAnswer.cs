e26b03c4cbda25152dcc53bc264a95cc3ca6a9d8
ngtduc693 <ngtduc693@gmail.com>
2024-11-09 02:00:08 +0700

    Add a feature to sync the model data

--- Models/QuestionAnswer.cs
+++ Models/QuestionAnswer.cs
@@ -1,7 +1,11 @@
-﻿namespace InsuranceBot.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InsuranceBot.Models
 {
     public class QuestionAnswer
     {
+        [Key]
+        public int Id { get; set; }
         public string Question { get; set; }
         public string Answer { get; set; }
         public float[] Embedding { get; set; }
